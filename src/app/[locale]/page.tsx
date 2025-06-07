26f7cc0ffb5265b5a0abe331b9ba3c564f8adbec
Em Evgeniy <emevgeniy1323@gmail.com>
2025-06-07 22:22:30 +0500

    feat(CODE-03): add fonts and favicon.icon

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -2,9 +2,10 @@ import { useTranslations } from 'next-intl';
 
 export default function Home() {
   const t = useTranslations('Home');
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>{t('title')}</p>
+      <p className="text-5xl">{t('title')}g</p>
     </div>
   );
 }
