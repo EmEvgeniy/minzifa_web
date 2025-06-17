df6bb220da6b6834fc30548fc7ef9a600e07e2e9
Timur Ashurov <56439783+timurashurov12@users.noreply.github.com>
2025-06-17 13:13:06 +0500

    fix(CODE-02): Update Wrapper.tsx

--- src/components/Home/Articles/Wrapper.tsx
+++ src/components/Home/Articles/Wrapper.tsx
@@ -3,19 +3,21 @@ import { useGetQuery } from '@/api/get.api';
 import { ArticleCard } from '@/components/UI';
 import { ArticleCardType } from '@/components/UI/ArticleCard/_types';
 import React from 'react';
+import { useLocale } from 'next-intl';
 
 interface DataResponse {
   data: ArticleCardType[];
 }
 
 export const Wrapper = () => {
+  const locale = useLocale();
   const { data, isSuccess } = useGetQuery<DataResponse>({
     key: ['articles'],
     page: '1',
     perPage: '3',
     url: 'articles',
     searchItem: '',
-    additionalParam: '',
+    additionalParam: `&locale=${locale}`,
   });
   return (
     <div className="grid grid-cols-3 w-full gap-5 [@media(max-width:768px)]:hidden">
