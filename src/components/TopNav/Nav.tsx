2ce701fd9ea0f6b52fb27e505187d5d1c5d4152a
Em Evgeniy <emevgeniy1323@gmail.com>
2025-07-03 03:01:11 +0500

    feat(CODE-135): remove prefetch link

--- src/components/TopNav/Nav.tsx
+++ src/components/TopNav/Nav.tsx
@@ -22,7 +22,7 @@ export const Nav: FC<NavMenuType> = ({ menu }) => {
           <Link
             href={`/${currentLocale}/${el.link}`}
             locale={currentLocale}
-            prefetch={true}
+            prefetch={false}
             className="flex items-center justify-center gap-2 hover:text-green-500 transition-all"
           >
             <span className=" text-nowrap text-[18px]">{el.title}</span>
