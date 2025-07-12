04c4240da1390459f403f58a3de8e78ef7a3f3da
Em Evgeniy <emevgeniy1323@gmail.com>
2025-07-12 16:19:10 +0500

    fix(CODE-02): fix popup alert

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import { NextIntlClientProvider } from 'next-intl';
 import { QueryProvider } from '@/providers/QueryProvider';
-import { ThemeProviderWrap } from '@/providers/ThemeProviderWrap';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { CustomSnackBar } from '@/components/UI/CustomSnackBar';
 import { UTMMetricsProvider } from '@/providers/UTMMetricsProvider';
 import dynamic from 'next/dynamic';
@@ -9,6 +9,7 @@ import TopNav from '@/components/TopNav/TopNav';
 import SocialMedia from '@/components/UI/SocialMedia/SocialMedia';
 import Footer from '@/components/Footer/Footer';
 import { getMessages } from 'next-intl/server';
+import { ThemeProviderWrap } from '@/providers/ThemeProviderWrap';
 
 const ClientPopupObserver = dynamic(() => import('@/layouts/ClientPopupObsorver'));
 
@@ -29,20 +30,22 @@ export default async function RootLayout({
   return (
     <NextIntlClientProvider locale={locale} messages={messages}>
       <QueryProvider>
-        <ThemeProviderWrap>
-          <UTMMetricsProvider>
-            <div className="flex flex-col h-full min-h-[100vh] w-full relative font-text">
-              <div className="hidden md:flex md:fixed md:right-0 md:z-50 md:bg-[rgba(22,55,45,0.7)] md:backdrop-blur-[6px] md:top-[150px] md:p-5 md:rounded-tl-[16px] md:rounded-bl-[16px] max-[1024px]:p-2.5">
-                <SocialMedia direction="vertical" gap={20} />
+        <AppRouterCacheProvider>
+          <ThemeProviderWrap>
+            <UTMMetricsProvider>
+              <div className="flex flex-col h-full min-h-[100vh] w-full relative font-text">
+                <div className="hidden md:flex md:fixed md:right-0 md:z-50 md:bg-[rgba(22,55,45,0.7)] md:backdrop-blur-[6px] md:top-[150px] md:p-5 md:rounded-tl-[16px] md:rounded-bl-[16px] max-[1024px]:p-2.5">
+                  <SocialMedia direction="vertical" gap={20} />
+                </div>
+                <TopNav locale={locale} />
+                <main className="flex-1">{children}</main>
+                <ClientPopupObserver locale={locale} />
+                <Footer locale={locale} />
+                <CustomSnackBar />
               </div>
-              <TopNav locale={locale} />
-              <main className="flex-1">{children}</main>
-              <ClientPopupObserver locale={locale} />
-              <Footer locale={locale} />
-              <CustomSnackBar />
-            </div>
-          </UTMMetricsProvider>
-        </ThemeProviderWrap>
+            </UTMMetricsProvider>
+          </ThemeProviderWrap>
+        </AppRouterCacheProvider>
       </QueryProvider>
     </NextIntlClientProvider>
   );
---
+++ src/app/[locale]/tours/loading.tsx
@@ -0,0 +1,11 @@
+function loading() {
+  return (
+    <div className="w-full min-h-screen h-full">
+      <div className="min-h-[70svh] w-full bg-white relative overflow-hidden">
+        <div className="absolute inset-0 animate-pulse bg-gradient-to-r from-[#16372D] via-[#245442] to-[#16372D] opacity-30 w-full h-full" />
+      </div>
+    </div>
+  );
+}
+
+export default loading;
