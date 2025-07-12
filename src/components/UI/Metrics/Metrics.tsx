1e400c42771624fbb3f5727752212c829b329c0d
Em Evgeniy <emevgeniy1323@gmail.com>
2025-07-12 16:26:38 +0500

    feat(CODE-123): add new script

--- src/components/UI/Metrics/Metrics.tsx
+++ src/components/UI/Metrics/Metrics.tsx
@@ -2,40 +2,39 @@ import { useLocale } from 'next-intl';
 import Script from 'next/script';
 
 export default function Metrics() {
-    const locale = useLocale();
-    const isRussian = locale === 'ru';
+  const locale = useLocale();
+  const isRussian = locale === 'ru';
 
-    return (
-        <>
-            {/* Universal Google Tag */}
-            <Script
-                async
-                src="https://www.googletagmanager.com/gtag/js?id=G-M136S4C9GK"
-            />
-            <Script id="google-tag-init" strategy="afterInteractive">
-                {`
+  return (
+    <>
+      {/* Universal Google Tag */}
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-M136S4C9GK" />
+      <Script id="google-tag-init" strategy="afterInteractive">
+        {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
 
-                    ${isRussian
+                    ${
+                      isRussian
                         ? `
                             gtag('config', 'AW-16625384260');
                         `
                         : `
                             gtag('config', 'G-M136S4C9GK');
-                        `}
+                        `
+                    }
 
                     gtag('config', 'AW-301071748');
                     gtag('config', 'G-JK3M1DVN37');
                 `}
-            </Script>
+      </Script>
 
-            {isRussian ? (
-                <>
-                    {/* Yandex Metrika */}
-                    <Script id="yandex-metrika" strategy="afterInteractive">
-                        {`
+      {isRussian ? (
+        <>
+          {/* Yandex Metrika */}
+          <Script id="yandex-metrika" strategy="afterInteractive">
+            {`
                             (function(m,e,t,r,i,k,a){
                                 m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
                                 m[i].l=1*new Date();
@@ -51,13 +50,13 @@ export default function Metrics() {
                                 ecommerce:"dataLayer"
                             });
                         `}
-                    </Script>
-                </>
-            ) : (
-                <>
-                    {/* Facebook Pixel */}
-                    <Script id="facebook-pixel" strategy="afterInteractive">
-                        {`
+          </Script>
+        </>
+      ) : (
+        <>
+          {/* Facebook Pixel */}
+          <Script id="facebook-pixel" strategy="afterInteractive">
+            {`
                             !function(f,b,e,v,n,t,s)
                             {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
                             n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
@@ -69,9 +68,18 @@ export default function Metrics() {
                             fbq('init', '777577019559963');
                             fbq('track', 'PageView');
                         `}
-                    </Script>
-                </>
-            )}
+          </Script>
         </>
-    );
+      )}
+      {!isRussian && (
+        <Script id="clarity" strategy="afterInteractive">
+          {`(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "sdnbxkik9r");`}
+        </Script>
+      )}
+    </>
+  );
 }
