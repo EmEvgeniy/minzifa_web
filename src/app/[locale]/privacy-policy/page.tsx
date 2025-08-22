118db545e353526fa86e21951d6ff3f69f181b02
timurasurov <timurasurov12@gmail.com>
2025-08-22 12:07:57 +0500

    feat(CODE-127): Add sertificates page

--- src/app/[locale]/privacy-policy/page.tsx
+++ src/app/[locale]/privacy-policy/page.tsx
@@ -4,6 +4,7 @@ import { Metadata } from 'next';
 import Breadcrumbs from '@/components/UI/Breadcrumbs/Breadcrumbs';
 import { DefaultPageProps } from '@/types';
 import { getTranslations } from 'next-intl/server';
+import Markdown from 'markdown-to-jsx';
 
 export function generateStaticParams() {
   return ['en', 'ru'].map((locale) => ({ locale }));
@@ -16,7 +17,7 @@ export async function generateMetadata({ params }: DefaultPageProps): Promise<Me
   const data = await fetch(
     `https://api.minzifatravel.com/api/v1/pages?page=${slug}`,
   ).then((res) => res.json());
-  
+
   return {
     title: data?.seo_metadata?.title,
     description: data?.seo_metadata?.description,
@@ -34,10 +35,9 @@ export default async function page({ params }: DefaultPageProps) {
       <h1 className="text-[42px] max-[768px]:text-[30px] max-[550px]:text-[24px] font-title">
         {t('privacy.title')}
       </h1>
-      <div
-        dangerouslySetInnerHTML={{ __html: t('privacy.text') || '' }}
-        className="text-[18px] max-[550px]:text-[14px]"
-      />
+      <Markdown className="text-[18px] max-[550px]:text-[14px]">
+        {t('privacy.text') || ''}
+      </Markdown>
     </section>
   );
 }
