82ee4e6f8602a9bedf0006f3e7007dac56dd3a69
blank2113 <blank2113@github.com>
2026-01-22 10:20:10 +0500

    add rules for protection branch

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,13 @@
-import type { MetadataRoute } from 'next'
+import type { MetadataRoute } from 'next';
 
+// default
 export default function robots(): MetadataRoute.Robots {
-    return {
-        rules: {
-            userAgent: '*',
-            allow: ['/en/', '/ru/'],
-            disallow: ['/en/booking/', '/ru/booking/'],
-        },
-        sitemap: ['https://minzifatravel.com/sitemap.xml'],
-    }
+  return {
+    rules: {
+      userAgent: '*',
+      allow: ['/en/', '/ru/'],
+      disallow: ['/en/booking/', '/ru/booking/'],
+    },
+    sitemap: ['https://minzifatravel.com/sitemap.xml'],
+  };
 }
