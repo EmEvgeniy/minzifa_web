5e03625826f92932d11613386ea74b9da87fbf35
Эм Евгений <141280778+EmEvgeniy@users.noreply.github.com>
2026-01-28 10:47:17 +0500

    Test (#6)
    * pl.js
    
    * fix(CODE-167): fixed linter errors
    
    * fix(CODE-168): add required phone input, fixed booking page in mobile version and fixed buttons in tour page in mobile view
    
    * text com
    
    * text com
    
    * Dockerfile (#4)
    
    * pl.js for dev
    
    * pl.js for dev2
    
    * Text (#3)
    
    * pl.js
    
    * fix(CODE-167): fixed linter errors
    
    * fix(CODE-168): add required phone input, fixed booking page in mobile version and fixed buttons in tour page in mobile view
    
    * text com
    
    ---------
    
    Co-authored-by: blank2113 <blank2113@github.com>
    Co-authored-by: Timur Ashurov <56439783+timurashurov12@users.noreply.github.com>
    
    * text com
    
    * add Dockerfile
    
    ---------
    
    Co-authored-by: blank2113 <blank2113@github.com>
    Co-authored-by: Timur Ashurov <56439783+timurashurov12@users.noreply.github.com>
    
    * test
    
    ---------
    
    Co-authored-by: blank2113 <blank2113@github.com>
    Co-authored-by: Timur Ashurov <56439783+timurashurov12@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 import bundleAnalyzer from '@next/bundle-analyzer';
 
-// Default config
 const withNextIntl = createNextIntlPlugin();
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
